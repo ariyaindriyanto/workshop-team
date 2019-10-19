cb2c1ce1abf03d0faced232d2ec6a25009f88463
kelasrobot <kelasrobot@gmail.com>
2019-10-19 14:09:32 +0700

    navbar

--- navbar.php
+++ navbar.php
@@ -1,22 +1,25 @@
 <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-info">
 <div class="container">
-  <a class="navbar-brand" href="#">Workshop GIT</a>
+  <a class="navbar-brand" href="#"><i class="fa fa-coffee"></i> Workshop GIT</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#"><i class="fa fa-home"></i> Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Subscribe</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link" href="#">Detail</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        <a class="nav-link" href="#">Feature</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Hero</a>
       </li>
     </ul>
   </div>
---
+++ readme.txt
@@ -0,0 +1,19 @@
+Install GIT Software (git-scm.com)
+Install VSCODE (Software yang direkomendasikan)
+Daftar Github.com 
+New Repositori
+copy URL GIT https://github.com/xxx/xxx.git
+Open folder, klik kanan Git Bash Here
+Terminal Git
+git clone <URL GIT>
+cd <nama folder / nama repositori>
+git config --global user.name “<username>”
+git config --global user.email “<email>”
+git add <nama file>
+git add .
+git rm -f <nama file>
+git commit -m “commitan”
+git push -u origin master
+git pull
+git checkout -b <nama file>
+git checkout <nama file>
