bc09d38632180717505647979ff3d947ba1c4119
fikrinaufal <fikrinaufal21@gmail.com>
2019-10-19 13:46:53 +0700

    hero-bg

---
+++ hero.php
@@ -0,0 +1,5 @@
+<div class="jumbotron text-center">
+    <h1 class="display-4 pt-4 pb-2">Hello, world!</h1>
+    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+</div>
