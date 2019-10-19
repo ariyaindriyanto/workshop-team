4f074ec30e4a2f37cbaf5fb805cbf4246306556e
gumilarraman <gumilarraman@gmail.com>
2019-10-19 14:36:12 +0700

    init

---
+++ subscibe.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+<h2 align="center">Ready to Started ? Get Sign Up !</h2>
+<div class="input-group flex-nowrap">
+  <div class="input-group-prepend">
+  </div>
+  <input type="text" class="form-control" placeholder="E-mail" aria-label="E-mail" aria-describedby="addon-wrapping" widht="10%">
+  <button type="button" class="btn btn-primary">Sign Up !</button>
+</div>
